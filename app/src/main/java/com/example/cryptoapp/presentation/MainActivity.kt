376510a63a0bd3d9463e1fe389157445fc4edeb3
ttakpotosu2 <itanjr@live.com>
2024-10-02 18:16:13 +0000

    edge to edge enabled

--- app/src/main/java/com/example/cryptoapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.cryptoapp.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
@@ -16,6 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
+        enableEdgeToEdge()
         setContent {
             CryptoAppTheme {
                 val navController = rememberNavController()
