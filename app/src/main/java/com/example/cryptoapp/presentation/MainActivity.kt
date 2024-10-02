4f00799902c5d33bb0b4a8ae01b43eb2790a3eb6
ttakpotosu2 <itanjr@live.com>
2024-10-02 18:07:00 +0000

    main activity imports

--- app/src/main/java/com/example/cryptoapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/MainActivity.kt
@@ -3,22 +3,11 @@ package com.example.cryptoapp.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
-import com.example.cryptoapp.presentation.coin_detail.CoinDetailScreen
-import com.example.cryptoapp.presentation.coin_tools.CoinToolsScreen
 import com.example.cryptoapp.presentation.coin_tools.viewmodels.CoinToolsViewModel
-import com.example.cryptoapp.presentation.coins_list.CoinsScreen
 import com.example.cryptoapp.presentation.navigation.NavGraph
-import com.example.cryptoapp.presentation.onboarding.OnBoardingScreen
 import com.example.cryptoapp.presentation.ui.theme.CryptoAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
